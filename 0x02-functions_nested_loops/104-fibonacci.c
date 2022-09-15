5f9bb43079d2fce15236496bef52bc2bf7b84862
KennedyOkoth <keinokoth.ok@gamil.com>
2022-09-14 19:32:25 -0700

    Done

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,46 +1,50 @@
 #include <stdio.h>
+
 /**
-*main - prints out first 98
-*fibonacci suit numbers
-*Return: return 0
-*/
+ * main - print first 98 Fibonacci numbers without using long long, malloc,
+ * pointers, array/tables, or structures
+ * Return: 0
+ */
+
 int main(void)
 {
-	int inc;
-	unsigned long n1 = 0, n2 = 1, n3;
-	unsigned long n1_h1, n1_h2, n2_h1, n2_h2;
-	unsigned long h1, h2;
+	int counter, overflow;
+	unsigned long a = 1;
+	unsigned long b = 1;
+	unsigned long sum = 0;
+	long a_head, a_tail, b_head, b_tail, sum_head, sum_tail;
 
-	for (inc = 0; inc < 92; inc++)
+	printf("1");
+
+	for (counter = 2; counter < 93; counter++) /* will still print */
 	{
-		n3 = n1 + n2;
-		printf("%lu, ", n3);
-		n1 = n2;
-		n2 = n3;
+		sum = a + b;
+		a = b;
+		b = sum;
+		printf(", %lu", sum);
 	}
-	n1_h1 = n1 / 10000000000;
-	n2_h1 = n2 / 10000000000;
-	n1_h2 = n1 % 10000000000;
-	n2_h2 = n2 % 10000000000;
-	for (inc = 93; inc < 99; inc++)
+
+	a_head = a / 1000000000; /* break larger num into 2 parts */
+	a_tail = a % 1000000000;
+	b_head = b / 1000000000;
+	b_tail = b % 1000000000;
+
+	for (; counter < 99; counter++)
 	{
-		h1 = n1_h1 + n2_h1;
-		h2 = n1_h2 + n2_h2;
-		if ((n1_h2 + n2_h2) > 9999999999)
-		{
-			h1 += 1;
-			h2 %= 10000000000;
-		}
-		printf("%lu%lu", h1, h2);
-		if (inc != 98)
-		{
-			printf(", ");
-			n1_h1 = n2_h1;
-			n1_h2 = n2_h2;
-			n2_h1 = h1;
-			n2_h2 = h2;
-		}
-	}	
+		overflow = (a_tail + b_tail) / 1000000000;
+		sum_tail = (a_tail + b_tail) - (1000000000 * overflow);
+		sum_head = (a_head + b_head) + overflow;
+
+		printf(", %lu%lu", sum_head, sum_tail);
+
+		a_head = b_head;
+		a_tail = b_tail;
+		b_head = sum_head;
+		b_tail = sum_tail;
+	}
+
+
 	printf("\n");
+
 	return (0);
 }
