c47d5d9e6fc8516a1530bfb2c952a2446e1401c3
KennedyOkoth <107689277+KennedyOkoth@users.noreply.github.com>
2024-04-09 15:57:39 +0300

    Update 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -7,7 +7,7 @@
  * @array: input array
  * @size: size of the array
  * @value: value to search in the array
- * Return: Always EXIT_SUCCESS
+ * Return: Index of the value in the array
  */
 int linear_search(int *array, size_t size, int value)
 {
